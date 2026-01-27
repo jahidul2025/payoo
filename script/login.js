d950ccafb428296a6530a58b8039ee236593a4cc
jahidul islam <jahidul20673@gmail.com>
2026-01-27 16:50:00 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document.getElementById("login-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const accountNumber = document.getElementById("Account-Number").value;
+    const pin = document.getElementById("Pin").value;
+    const convertedPin = parseInt(pin)
+
+    if (accountNumber.length === 11) {
+        if (convertedPin === 1234) {
+            window.location.href = "main.html"
+        }
+        else {
+            console.log("pin tikh nay")
+        }
+    }
+    else {
+        console.log("need valid account number")
+    }
+
+})
